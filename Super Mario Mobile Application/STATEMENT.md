ebc7708901d827638f7deb00ba4ce9b62a7941b7
ameer117 <50126669+ameer117@users.noreply.github.com>
2019-11-29 19:02:33 -0800

    Add files via upload

---
+++ Super Mario Mobile Application/STATEMENT.md
@@ -0,0 +1,2 @@
+# ASSIGNMENT--4
+NOT MY OWN CODE I followed a youtube series for guidance and modified the example code to fit the assignment requirements.
---
+++ Super Mario Mobile Application/core/src/com/ameer/mariobros/Sprites/TileObjects/Brick.java
@@ -0,0 +1,30 @@
+package com.ameer.mariobros.Sprites.TileObjects;
+
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.math.Rectangle;
+import com.ameer.mariobros.MarioBros;
+import com.ameer.mariobros.Scenes.Hud;
+import com.ameer.mariobros.Screen.PlayScreen;
+import com.ameer.mariobros.Sprites.Mario;
+
+
+public class Brick extends InteractiveTileObject {
+    public Brick(PlayScreen screen, MapObject object){
+        super(screen, object);
+        fixture.setUserData(this);
+        setCategoryFilter(MarioBros.BRICK_BIT);
+    }
+
+    @Override
+    public void onHeadHit(Mario mario) {
+        if(mario.isBig()) {
+            setCategoryFilter(MarioBros.DESTROYED_BIT);
+            getCell().setTile(null);
+            Hud.addScore(200);
+            MarioBros.manager.get("audio/sounds/breakblock.wav", Sound.class).play();
+        }
+        MarioBros.manager.get("audio/sounds/bump.wav", Sound.class).play();
+    }
+
+}
